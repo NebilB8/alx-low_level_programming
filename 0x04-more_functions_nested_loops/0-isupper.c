9dcc09d9241fd90365e1a7d847e923164d1ac1e4
NebilB8 <85708227+NebilB8@users.noreply.github.com>
2022-06-11 16:23:41 +0300

    Add files via upload

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,10 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
- int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-
-	else
-		return (0);
-}
  
 int main(void)
 {
